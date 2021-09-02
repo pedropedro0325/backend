593f2e31e75d2c727ef33fa30fefbab74ccf24db
foudus <foudaatanganaj@gmail.com>
2021-09-02 20:51:21 +0100

    first commit

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -4,7 +4,16 @@ module.exports= (app) => {
 
 //create a new note
     app.post('/notes', notes.create);
-}
+
 
 //Retrieve all notes
+app.get('/notes',notes.findAll);
+
+//Retrieve a single Note with noteId
+app.get('/notes/:noteId',notes.findOne);
+//Update a Note  with noteId
+app.put('/notes/:notesId',notes.update);
+//Delete a Note with noteId
+app.delete('/notes/:noteId',notes.delete);
+}
 //TODO
